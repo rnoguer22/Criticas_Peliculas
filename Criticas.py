08c42728c535c49ae29607e2fab5a83050ab8eb8
rnoguer22 <rnogugon@myuax.com>
2022-04-25 19:42:24 +0200

    Grafico de barras con los datos del enunciado

--- Criticas.py
+++ Criticas.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 datos = {
     "Opinion":[5,4,3,2,1,0],
@@ -7,3 +8,11 @@ datos = {
 
 #Creamos el dataframe a partir de los datos del enunciado
 dt = pd.DataFrame(datos)
+print (dt)
+
+#Realizamos el grafico barras a partir de los datos del enunciado
+dt.groupby("Opinion")["Cantidad de Votantes"].sum().plot(kind="bar")
+#Añadimos un titulo para el grafico
+plt.title("Opiniones obtenidas para una pelicula")
+plt.show()
+
