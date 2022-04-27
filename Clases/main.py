38020209956d11ddfa1e04234218262476a4a209
rnoguer22 <rnogugon@myuax.com>
2022-04-27 20:18:38 +0200

    Calculo de la desviacion estandar

--- Clases/main.py
+++ Clases/main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from grafico import Grafico
 from media import Media
+from std import Std
 
 #El csv contiene algun error, por lo que no se puede "jugar" con el
 dt = pd.read_csv("reviews_filmaffinity.csv")
@@ -12,4 +13,8 @@ grafico.grafico_barras("Critica Peliculas Españolas")
 #No obstante esto es orientativo, unicamente para trabajar con un dataset grande
 media = Media(dt,"film_avg_rate","review_rate")
 print ("La media es {}".format(media.calcular_media()))
+
+#Ocurre lo mismo que con la media en el caso de la desviacion estandar
+desviacion_estandar = Std(dt, "film_avg_rate","review_rate")
+print ("La desviacion estandar es {}".format(desviacion_estandar))
 
