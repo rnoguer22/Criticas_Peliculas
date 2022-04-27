8a33f40a8d3d4c26a2e960b5686265063bf10b2f
rnoguer22 <rnogugon@myuax.com>
2022-04-27 18:34:57 +0200

    Solucion error en el metodo porcentaje

--- Clases/porcentaje.py
+++ Clases/porcentaje.py
@@ -11,12 +11,13 @@ class Porcentaje(Media):
         self.desviacion = Std(dataset, col1, col2)
 
     def porcentaje(self, num):
-        lim_inf = self.media - num*self.desviacion
-        lim_sup = self.media + num*self.desviacion
+        lim_inf = self.media.calcular_media() - num*self.desviacion.calcular_std()
+        lim_sup = self.media.calcular_media() + num*self.desviacion.calcular_std()
         r = []
         s = []
+        indice = len(self.dataset) - 1
         for i in range (len(self.dataset)):
-            if self.dataset[self.col1][len(self.dataset)-i] >= lim_inf and self.dataset[self.col1][5-i] <= lim_sup:
+            if self.dataset[self.col1][indice-i] >= lim_inf and self.dataset[self.col1][indice-i] <= lim_sup:
                 r.append(self.dataset[self.col1][i])
                 s.append(self.dataset[self.col2][i])
             else:
@@ -26,15 +27,3 @@ class Porcentaje(Media):
 
         #Hallamos el porcentaje
         print ("El porcentaje es {}%".format(round(sum(s)/self.dataset[self.col2].sum(), 2)))
-
-
-datos = {
-    "Opinion":[5,4,3,2,1,0],
-    "Cantidad de Votantes":[42,96,132,124,88,58]
-}
-
-#Creamos el dataframe a partir de los datos del enunciado
-dt = pd.DataFrame(datos)
-
-por = Porcentaje(dt, "Opinion", "Cantidad de Votantes")
-por.porcentaje(1)
